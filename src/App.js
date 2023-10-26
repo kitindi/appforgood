0c9f9ebf342e1e115aa493e77ede13ac01efa529
kitindi <abdullkitindi@gmail.com>
2023-10-26 07:22:01 +0300

    a clean initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">React Tutorial</h1>
+      <p>
+        Welcome to the one most rated website that can transform you from novice
+        to pro react developer
+      </p>
+      <button className="btn"> Sign up now for early access</button>
     </div>
   );
 }
